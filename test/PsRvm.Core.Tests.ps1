8dbb3a59c0271da2cd9f46420c95929a90a29b21
Craig Meinschein <cmeinsch@gmail.com>
2015-10-03 14:51:24 -0700

    Initial commit of psrvm module, with tests!

---
+++ test/PsRvm.Core.Tests.ps1
@@ -0,0 +1,10 @@
+﻿$srcFile = $MyInvocation.MyCommand.Path `
+        -replace 'psrvm\\test\\(.*)\.Tests\.ps1', `
+                 'psrvm\src\$1.ps1'
+    . $srcFile
+
+Describe 'PsRvm.Core' {
+    It 'should exist' {
+        psrvm | Should Be (-not $null)
+    }
+}
---
+++ test/PsRvm.Module.Tests.ps1
@@ -0,0 +1,8 @@
+﻿$moduleDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\.."
+Import-Module "$moduleDir\psrvm.psd1"
+
+Describe "PsRvm.Module" {
+    It 'should exist' {
+        Get-Module psrvm | Should Be (-not $null)
+    }
+}
