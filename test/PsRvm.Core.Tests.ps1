213a89d9e35added2731f1bcbcfee69337d571c2
Craig Meinschein <cmeinsch@gmail.com>
2015-10-03 16:05:51 -0700

    Fix tests and module file encoding

--- test/PsRvm.Core.Tests.ps1
+++ test/PsRvm.Core.Tests.ps1
@@ -1,10 +1,11 @@
-﻿$srcFile = $MyInvocation.MyCommand.Path `
+﻿if (Get-Module psrvm) { Remove-Module psrvm }
+$srcFile = $MyInvocation.MyCommand.Path `
         -replace 'psrvm\\test\\(.*)\.Tests\.ps1', `
                  'psrvm\src\$1.ps1'
-    . $srcFile
+. $srcFile
 
 Describe 'PsRvm.Core' {
-    It 'should exist' {
-        psrvm | Should Be (-not $null)
+    It 'should provide the psrvm command' {
+        Get-Command psrvm | Should Not BeNullOrEmpty
     }
 }
--- test/PsRvm.Module.Tests.ps1
+++ test/PsRvm.Module.Tests.ps1
@@ -1,8 +1,13 @@
-﻿$moduleDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\.."
+﻿if (Get-Module psrvm) { Remove-Module psrvm }
+$moduleDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\.."
 Import-Module "$moduleDir\psrvm.psd1"
 
 Describe "PsRvm.Module" {
     It 'should exist' {
-        Get-Module psrvm | Should Be (-not $null)
+        Get-Module psrvm | Should Not BeNullOrEmpty
+    }
+    It 'should load PsRvm.Core commands' {
+        @((Get-Module psrvm).NestedModules | Select -Expand Name) `
+            -contains 'PsRvm.Core' | Should Be $true
     }
 }
