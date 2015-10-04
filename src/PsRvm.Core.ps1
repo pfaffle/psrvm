7e44053d8d07b1f6ca47e07ef3e40a5bd6c3b1a8
Craig Meinschein <cmeinsch@gmail.com>
2015-10-04 13:40:04 -0700

    Add support for fetching and parsing ruby version numbers.

--- src/PsRvm.Core.ps1
+++ src/PsRvm.Core.ps1
@@ -18,3 +18,24 @@ function _get_native_arch {
         return 'i386'
     }
 }
+
+function _get_latest_ruby {
+    $versions = _get_available_ruby_versions | Sort -Descending
+    return $versions[0]
+}
+
+function _get_available_ruby_versions {
+    $html = (_get_web_client).DownloadString('http://dl.bintray.com/oneclick/rubyinstaller/') -split "`n"
+    $versions = @()
+    foreach ($line in $html) {
+        if ($line -match '\<pre\>\<a onclick="navi\(event\)" href="\:rubyinstaller\-(\d\.\d\.\d(\-p\d+)?)\.exe') {
+            $versions += $matches[1]
+        }
+    }
+    return $versions | Sort -Unique
+}
+
+# For testing
+function _get_web_client {
+    return New-Object System.Net.WebClient
+}
--- test/PsRvm.Core.Tests.ps1
+++ test/PsRvm.Core.Tests.ps1
@@ -23,6 +23,34 @@ function UndoMockArch {
     Mock Test-Path -ParameterFilter {$Path -eq "$env:systemroot\syswow64"} `
                    -MockWith { Test-Path "$env:systemroot\syswow64" }
 }
+function GetMockWebClient {
+    $MockWebClient = New-Object PSCustomObject
+    $MockWebClient | Add-Member -MemberType ScriptMethod -Name DownloadString -Value {
+        param([string]$address)
+        return Get-Content "$rootDir\res\test\rubyinstaller_list.html" | Out-String
+    }
+    return $MockWebClient
+}
+function HasValues {
+    param(
+    [CmdletBinding()]
+    [String[]]$ExpectedValues,
+    [Parameter(ValueFromPipeline=$true)]
+    [String]$Actual
+    )
+    PROCESS {
+        $ActualValues += @($Actual)
+    }
+    END {
+        foreach ($Expected in $ExpectedValues) {
+            if ($ActualValues -notcontains $Expected) {
+                Write-Error "{$Expected} expected but was not encountered.`nActual: {$ActualValues}"
+                return $false
+            }
+        }
+        return $true
+    }
+}
 
 # Tests
 Describe 'PsRvm.Core' {
@@ -32,20 +60,46 @@ Describe 'PsRvm.Core' {
     It 'should install to a default path if one is not specified' {
         Install-Ruby | Should Be "$env:userprofile\.psrvm"
     }
+}
 
-    Context '_get_native_arch' {
-        AfterEach {
-            UndoMockArch
+Describe '_get_native_arch' {
+    AfterEach {
+        UndoMockArch
+    }
+    It 'returns i386 on a 32-bit system' {
+        Mock32BitArch
+        # I should really be asserting that these mocks are called, but
+        # it isn't working and I can't figure out why.
+        _get_native_arch | Should Be 'i386'
+    }
+    It 'returns x64 on a 64-bit system' {
+        Mock64BitArch
+        _get_native_arch | Should Be 'x64'
+    }
+}
+
+Describe '_get_web_client' {
+    It 'returns a new System.Net.WebClient object' {
+        (_get_web_client).GetType().FullName | Should Be 'System.Net.WebClient'
+    }
+}
+
+Describe '_get_available_ruby_versions' {
+    Context 'With Ruby installers for 1.9.2-p0, 1.9.2-p290, 1.9.3-p551, and 2.2.3' {
+        Mock _get_web_client -MockWith {GetMockWebClient}
+
+        It 'returns all available versions' {
+            _get_available_ruby_versions | HasValues @('1.9.2-p0', '1.9.2-p290', '1.9.3-p551', '2.2.3') | Should Be $true
         }
-        It 'returns i386 on a 32-bit system' {
-            Mock32BitArch
-            # I should really be asserting that these mocks are called, but
-            # it isn't working and I can't figure out why.
-            _get_native_arch | Should Be 'i386'
-        }
-        It 'returns x64 on a 64-bit system' {
-            Mock64BitArch
-            _get_native_arch | Should Be 'x64'
+    }
+}
+
+Describe '_get_latest_ruby' {
+    Context 'With Ruby installers for 1.9.2-p0, 1.9.2-p290, 1.9.3-p551, and 2.2.3' {
+        Mock _get_web_client -MockWith {GetMockWebClient}
+
+        It 'returns 2.2.3' {
+            _get_latest_ruby | Should Be '2.2.3'
         }
     }
 }
